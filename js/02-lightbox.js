5923e0da739ff4e7a490eaebb772bd217a1c3385
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-03-06 13:20:44 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery')
+const imagesCardsMarkup = createImgCardsMarkup(galleryItems)
+
+// Create img markup
+function createImgCardsMarkup(images) {
+    return images.map(({preview, original, description}) => {
+        return `
+            <a class="gallery__item" onclick="event.preventDefault()" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+    }).join('')
+}
+
+// Insert img markup
+galleryContainer.insertAdjacentHTML('beforeend', imagesCardsMarkup)
+
+const gallery = new SimpleLightbox(".gallery a", {
+	captions: true,
+	captionSelector: "img",
+	captionType: "attr",
+	captionsData: "alt",
+	captionPosition: "bottom",
+	captionDelay: 250,
+});
+
